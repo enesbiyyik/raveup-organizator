6d84740414d6aae5ce8705179c80f789eb9bc6f9
enesbiyyik <enes.biyyik@outlook.com>
2023-02-09 21:44:46 +0300

    works pretty good on android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_interpolation_to_compose_strings
+
+import 'dart:convert';
+
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'package:raveup_qr_reader/checked.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:raveup_qr_reader/constants.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,45 +41,106 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String data = "";
+  late Map<String, dynamic> ticket;
 
   @override
   Widget build(BuildContext context) {
+    ticket = jsonDecode(data);
     return Scaffold(
       backgroundColor: appPrimaryDark,
       appBar: AppBar(
         title: const Text("RaveUp Organizator"),
         backgroundColor: appPrimaryLight,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            TextButton(
-              onPressed: (){
-                debugPrint("qr modu");
-                scanQrCode();
-                debugPrint(data);
-              },
-              child: const Text("QR Okut"),
-            ),
-            TextButton(
-              onPressed: (){
-                debugPrint(data);
-                debugPrint("giriş yap");
-                Navigator.push(context, MaterialPageRoute(builder: (context) => PassCheckPage(data)));
-              }, 
-              child: const Text("Giriş Yap")
-            ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: appDetailPurple,
+        onPressed: (){
+          scanQrCode();
+          ticket = jsonDecode(data);
+        },
+        label: Row(
+          children: const [
+            Icon(Icons.camera_alt),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Bilet Okut"),
+            )
           ],
+        ),
+      ),
+      body: Center(
+        child: data == "" ? Container() : Center(
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 32, bottom: 32, left: 32, right: 32),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("İsim:", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                            AutoSizeText(ticket["participantName"], style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold), maxLines: 1,),
+                          ],
+                        ),
+                        const Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("Etkinlik:", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                            Expanded(
+                              child: AutoSizeText(ticket["eventName"], style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold), maxLines: 2, textAlign: TextAlign.end,),
+                            ),
+                          ],
+                        ),
+                        const Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("Tarih:", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                            Text(ticket["eventDate"], style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                        const Divider(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text("Onay:", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                            Text(ticket["isApproved"] ? "Evet" : "Hayır", style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Center(
+                      child: ticket["isApproved"] ? const Icon(Icons.task_alt, color: Colors.green, size: 48) : const Icon(Icons.disabled_by_default, color: Colors.red, size: 48,),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
 
   void scanQrCode() {
-    FlutterBarcodeScanner.scanBarcode("#3A3563", "Cancel", true, ScanMode.BARCODE).then((value) {
+    FlutterBarcodeScanner.scanBarcode("#3A3563", "Geri Çık", false, ScanMode.BARCODE).then((value) {
       setState(() {
-        debugPrint(value);
-        this.data = value;
+        data = value;
       });
     });
   }
